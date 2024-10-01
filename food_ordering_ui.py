98da87a635d09617a948ce93d32fb7d7eb28a83c
kavisana <kavisana@murraystate.edu>
2024-10-01 15:41:47 -0500

    Add files via upload

---
+++ food_ordering_ui.py
@@ -0,0 +1,41 @@
+#user interface to the main menu
+import data
+import functions
+def show_main_menu():
+  while True:
+    print("Solomon diner") #edit to show your name
+    print("__________")
+    print('N for a new order')
+    print('X for close orders and print the check')
+    print('Q for quit')
+    user_menu_choice = input('Your choice: ')
+    if user_menu_choice in 'Qq':
+      break
+    elif user_menu_choice in 'Xx':
+      print('This option prints the list of items ordered, extended price, total, Taxes, and Grand total ')
+    elif user_menu_choice in 'Nn':
+      print('New order')
+      make_order(user_menu_choice.upper())  #calls a function for adding to the orders
+
+def make_order(menu_choice):
+  print('Functionality for menu choice ', menu_choice)
+  user_selection = functions.get_item_number()
+  item_code, quantity = user_selection.split()
+  print(functions.get_item_information(item_code))
+
+def close_order(menu_choice):
+  print('Functionality for menu choice ', menu_choice)
+
+
+
+if __name__ == '__main__':
+    #initialize the lists
+    drinks = []
+    appetizers = []
+    salads = []
+    entrees = []
+    dessert= []
+    #print(functions.get_item_information('D1'))
+    show_main_menu()
+
+
